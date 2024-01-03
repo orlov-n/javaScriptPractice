44c7fd02dc5e353737b15b1966575eb3128d7723
Nick O <ialwaysreply@gmail.com>
2024-01-02 21:31:31 -0700

    Finish video-game

--- video-games/video-games.js
+++ video-games/video-games.js
@@ -1,4 +1,39 @@
+const createPlayer = (name, age, moveset) => {
+return {
+    name,
+    age,
+    moveset,
+}
+}
 
+const createLevel = (levelName, player) => {
 
+    console.log(levelName)
+return {
+    name: levelName,
+    players: player,
+    coins: 0,
+    lives: 3,
+}
+}
 
-module.exports = { };
+const findCoins = (level, coins) => {
+console.log(level)
+level.coins += coins
+if(level.coins >= 100) {
+    level.lives += 1
+}
+return level
+}
+
+const defeatPlayer = (level) => {
+level.lives -= 1
+return level.lives >= 1 ? level : "GAME OVER"
+}
+
+module.exports = { 
+    createPlayer,
+    createLevel,
+    findCoins,
+    defeatPlayer,
+};
