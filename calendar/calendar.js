b140a8b4d73a5ce2c18dc476031b002eee17b69c
Nick O <ialwaysreply@gmail.com>
2024-01-02 22:12:10 -0700

    Finish calendar

--- calendar/calendar.js
+++ calendar/calendar.js
@@ -1,3 +1,34 @@
+const createEvent = (title, month, day) => {
+return day > 0 && day < 31 ? {
+    title,
+    month,
+    day,
+} : `Error: ${day} is not a valid day`
+}
 
 
-module.exports = {  };
+const createCalendar = (owner, events) => {
+
+    return {
+        owner,
+        events
+    }
+}
+
+const reportMonthlyEvents = (calendar, month) => {
+    let events = []
+    calendar.events.forEach(event => {
+        if(event.month === month) {
+
+            events.push(event)
+        }
+    })
+    //console.log(events)
+    return events
+}
+
+module.exports = { 
+    createEvent, 
+    createCalendar, 
+    reportMonthlyEvents 
+};
