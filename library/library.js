784b2cdbbf8aa1c3bb193a9fa697ba8ff1d0e554
Nick O <ialwaysreply@gmail.com>
2024-01-03 21:46:10 -0700

    Finish library

--- library/library.js
+++ library/library.js
@@ -1,3 +1,22 @@
+const issueCard = (name, age) => {
+return {
+    name,
+    age, 
+   numBooksCheckedOut: 0,
+   isChild: age < 12,
+}
+}
 
+const searchByAuthor = (collection, name) => {
+//console.log(collection)
+let foundBook = collection.filter(book => book.author === name)
+console.log('thi is found book', foundBook)
+return foundBook.length < 1 ? 'No book found for search criteria' : foundBook
 
-module.exports = { };
+ 
+}
+
+module.exports = { 
+    issueCard,
+    searchByAuthor,
+};
