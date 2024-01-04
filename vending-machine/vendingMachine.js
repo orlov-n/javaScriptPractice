c1e479b22fcb7a6d5ea7912df76fcc95652e8911
Nick O <ialwaysreply@gmail.com>
2024-01-03 18:56:37 -0700

    Finish vending machine

--- vending-machine/vendingMachine.js
+++ vending-machine/vendingMachine.js
@@ -1,2 +1,30 @@
+const createItemStock = (name, quantity, price) => {
+return {
+    name: name || 'unknown',
+    quantity: quantity || 0,
+    price: price || 1.00
+}
+}
 
-module.exports = {}
+const makePurchase = (item, money) => {
+if(item.price > money) {
+     return `Sorry, you need at least $${item.price} to make that purchase`
+} else if(item.quantity < 1) {
+    return "Sorry, there are none left"
+} else {
+    return `Here are your ${item.name}`
+}
+return
+}
+
+const collectChange = (money) => {
+    // let total = null
+    // console.log(money)
+    return money.reduce((acc, num) => acc + num, 0)
+}
+
+module.exports = {
+    createItemStock,
+    makePurchase,
+    collectChange,
+}
