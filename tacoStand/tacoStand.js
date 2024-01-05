ea2b78388147fc5f2ea3c0848e7a1f7ce54354d4
Nick O <ialwaysreply@gmail.com>
2024-01-04 22:09:50 -0700

    Finish tacoStand

--- tacoStand/tacoStand.js
+++ tacoStand/tacoStand.js
@@ -1,2 +1,34 @@
+const createIngredient = (name, price) => {
+return {
+    name: name || 'unknown',
+    price: price || 0.00,
+}
+}
 
-module.exports = {}
+const createTaco = (name, ingredients) => {
+   // console.log(ingredients)
+return {
+    name: name || 'custom',
+    ingredients: ingredients || [],
+}
+}
+
+
+const addIngredientToTaco = (name, ingredient) => {
+    name.ingredients.push(ingredient)
+return name
+}
+
+const calculatePrice = (taco) => {
+    console.log();
+console.log(taco);
+return taco.ingredients.reduce((acc, item)=> acc + item.price, 0)
+ 
+}
+
+module.exports = {
+    createIngredient,
+    createTaco,
+    addIngredientToTaco,
+    calculatePrice,
+}
