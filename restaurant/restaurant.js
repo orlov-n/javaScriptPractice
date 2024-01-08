8de46dd36071ea03573e3af6050f0cb76855fb36
Nick O <ialwaysreply@gmail.com>
2024-01-07 22:06:37 -0700

    Finish restaurant

--- restaurant/restaurant.js
+++ restaurant/restaurant.js
@@ -1,11 +1,62 @@
+const createMeal = (name, specialRequests, tableNumber) => {
+  return {
+    name, 
+    specialRequests,
+     tableNumber,
+     complete: false,
+  }
+}
 
+const getMade = (meal) => {
+  meal.complete = true
+return meal
+}
+
+const announceMeal = (meal) => {
+if (meal.complete) {
+  return `Order up - ${meal.name} for table ${meal.tableNumber}!`
+} 
+return `This ${meal.name} is not completed yet`
+}
+
+
+const createOrder = (order) => {
+//console.log(order);
+return {
+  tableNumber: order.name,
+  meals: order.meals,
+  completedMeals: [],
+}
+}
+
+const cookMeal = (order, meal) => {
+  //console.log(meal)
+  if (order.tableNumber === meal.tableNumber){
+
+    order.completedMeals.push(meal.name)
+  }
+return order
+}
+
+
+const listOrders = (order) => {
+  return order.meals.map(meal => meal.name)
+}
+const listSpecialRequests = (order) => {
+  let requests = []
+  console.log(order);
+   order.meals.forEach(meal => {
+    meal.specialRequests.forEach(request => requests.push(request))
+  })
+return requests
+}
 
 module.exports = { 
-  // createMeal, 
-  // getMade, 
-  // announceMeal,
-  // createOrder, 
-  // cookMeal, 
-  // listOrders, 
-  // listSpecialRequests
+  createMeal, 
+  getMade, 
+   announceMeal,
+   createOrder, 
+   cookMeal, 
+  listOrders, 
+  listSpecialRequests
 };
